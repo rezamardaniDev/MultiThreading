70ec5fd5036005d5ee864d0e5f638d01eab82aa1
reza <mardanireza58@gmail.com>
2023-09-16 11:44:20 +0330

    update

---
+++ semaphore.py
@@ -0,0 +1,40 @@
+from threading import Thread, Semaphore, current_thread, BoundedSemaphore
+from time import sleep
+
+num = 0
+lock = Semaphore(value=2)
+b_lock = BoundedSemaphore(value=2)
+
+
+def add():
+    global num
+    lock.acquire()
+    print(current_thread().name)
+    sleep(2)
+    num += 1
+    lock.release()
+    lock.release()
+
+
+t1 = Thread(target=add)
+t2 = Thread(target=add)
+t3 = Thread(target=add)
+t4 = Thread(target=add)
+t5 = Thread(target=add)
+t6 = Thread(target=add)
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+t6.start()
+
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+t6.join()
+
+print(num)
